90d7ad990e103e2f2e6145800072058fbc1c01e0
Will <wdefine@students.stab.org>
2016-03-30 14:03:59 -0400

    We may be done

--- update.js
+++
@@ -1,8 +0,0 @@
-var anyDB = require('any-db');
-
-var conn = anyDB.createConnection('sqlite3://rivers.db.sqlite');
-
-conn.query('UPDATE stats SET ($1) = ($2) WHERE ident = ($3)', ["temperature", 30, 2]);
-conn.query('UPDATE stats SET ($1) = ($2) WHERE ident = ($3)', ["flow_rate", 5, 2]);
-conn.query('UPDATE stats SET ($1) = ($2) WHERE ident = ($3)', ["phosphates", 4.0, 2]);
-conn.query('UPDATE stats SET ($1) = ($2) WHERE ident = ($3)', ["ph", 6.55, 2]);
